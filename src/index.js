366421993583fe899c4637f9b81b6c2ef86773f5
Ali Sariyer <alisariyer@gmail.com>
2022-03-22 21:51:24 +0000

    Add persistance for the dropped element

--- src/index.js
+++ src/index.js
@@ -1,23 +1,73 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import Column from './Column';
-import { DragDropContext } from 'react-beautiful-dnd';
-import initialData from './initialData';
-import 'normalize.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Column from "./Column";
+import { DragDropContext } from "react-beautiful-dnd";
+import initialData from "./initialData";
+import "normalize.css";
 
 /**
  * react-beautiful-dnd has 3 major terms:
  * DragDropContext where we can move the object
  * Droppable where we can drop the object
  * Draggable is the object we want to drag and drop
- * 
+ *
  */
 
 const App = () => {
   const [data, setData] = useState(initialData);
 
-  const onDragEnd = () => {
-    // TODO
+  /**
+   * result give an object like this:
+   * {
+   *    draggableId: 'task-1',
+   *    type: 'TYPE',
+   *    reason: 'DROP',
+   *    source: {
+   *      droppableId: 'column-1',
+   *      index: 0,
+   *    },
+   *    destination: {
+   *      droppableId: 'column-1',
+   *      index: 1,
+   *    }
+   * }
+   *
+   * Note: Some cases destination can be null as drop outside
+   *
+   */
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    console.log(newColumn)
+
+    setData(prevData => {
+      return {
+        ...prevData,
+        columns: {
+          ...prevData.columns,
+          [newColumn.id]: newColumn,
+        }
+      }
+    })
   }
 
   return (
@@ -26,20 +76,18 @@ const App = () => {
       // onDragStart, onDragUpdate, onDragEnd
       onDragEnd={onDragEnd}
     >
-      {data.columnOrder.map(columnId => {
+      {data.columnOrder.map((columnId) => {
         const column = data.columns[columnId];
         const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
-        return <Column key={column.id} column={column} tasks={tasks}/>
+        return <Column key={column.id} column={column} tasks={tasks} />;
       })}
     </DragDropContext>
-  )
-}
-
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
